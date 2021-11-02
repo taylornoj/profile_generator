904bfd79b3b71552b085c9ef0e70bd054bb0f3b5
Taylor Noj <taylornoj@gmail.com>
2021-11-02 23:27:04 +0000

    adding questions to the survey

--- survey.js
+++ survey.js
@@ -5,8 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
 
+  rl.question('What\'s an activity you like doing? ', (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+    rl.question('What do you listen to while doing that? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+
   rl.close();
+});
+});
+});
 });
